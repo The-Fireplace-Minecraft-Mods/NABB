a0b4e88e4549968151645c0c61f435404ff1e267
The_Fireplace <the.f1repl4ce@gmail.com>
2016-07-01 23:19:12 -0500

    First attempt at animation

--- src/main/java/the_fireplace/nabb/NABB.java
+++ src/main/java/the_fireplace/nabb/NABB.java
@@ -5,9 +5,16 @@ import forestry.api.apiculture.BeeManager;
 import forestry.api.apiculture.EnumBeeType;
 import forestry.api.apiculture.IBee;
 import forestry.apiculture.PluginApiculture;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumHandSide;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -76,14 +83,32 @@ public class NABB {
     private boolean a1 = false;
     private boolean a2 = false;
     private boolean a3 = false;
+    float equippedProgress = 0.0F;
+    float prevEquippedProgress = 0.0F;
     public int ticksUsing = 0;
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public void renderHand(RenderHandEvent event){
         if(flag){
-            //TODO Hand move code here
-
+            //Hand move code here//Not gonna lie, most of the code I am trying here starts off copy-pasted from ItemRenderer.class.
+            /*AbstractClientPlayer abstractclientplayer = Minecraft.getMinecraft().thePlayer;
+            float par3 = abstractclientplayer.prevRotationPitch + (abstractclientplayer.rotationPitch - abstractclientplayer.prevRotationPitch) * event.getPartialTicks();//TODO determine these 3 variables
+            float par7 = 1.0F - (prevEquippedProgress + (equippedProgress - prevEquippedProgress) * event.getPartialTicks());
+            float par5 = abstractclientplayer.getSwingProgress(event.getPartialTicks());
+            float f = MathHelper.sqrt_float(par5);
+            float f1 = -0.2F * MathHelper.sin(par5 * (float)Math.PI);
+            float f2 = -0.4F * MathHelper.sin(f * (float)Math.PI);
+            GlStateManager.translate(0.0F, -f1 / 2.0F, f2);
+            float f3 = getMapAngleFromPitch(par3);
+            GlStateManager.translate(0.0F, 0.04F + par7 * -1.2F + f3 * -0.5F, -0.72F);
+            GlStateManager.rotate(f3 * -85.0F, 1.0F, 0.0F, 0.0F);
+            renderArms();
+            float f4 = MathHelper.sin(f * (float)Math.PI);
+            GlStateManager.rotate(f4 * 20.0F, 1.0F, 0.0F, 0.0F);
+            GlStateManager.scale(2.0F, 2.0F, 2.0F);*/
+            transformEatFirstPerson(event.getPartialTicks(), EnumHandSide.LEFT);
+            transformEatFirstPerson(event.getPartialTicks(), EnumHandSide.RIGHT);
             //End hand move code
             if(!reverse)
                 ticksUsing++;
@@ -107,5 +132,70 @@ public class NABB {
                 a3=false;
             }
         }
+        prevEquippedProgress = equippedProgress;
+        float f = 0.4F;
+        equippedProgress+= MathHelper.clamp_float((f * f * f) - equippedProgress, -0.4F, 0.4F);
+    }
+    private float getMapAngleFromPitch(float pitch)
+    {
+        float f = 1.0F - pitch / 45.0F + 0.1F;
+        f = MathHelper.clamp_float(f, 0.0F, 1.0F);
+        f = -MathHelper.cos(f * (float)Math.PI) * 0.5F + 0.5F;
+        return f;
+    }
+    private void renderArms()
+    {
+        if (!Minecraft.getMinecraft().thePlayer.isInvisible())
+        {
+            GlStateManager.disableCull();
+            GlStateManager.pushMatrix();
+            GlStateManager.rotate(90.0F, 0.0F, 1.0F, 0.0F);
+            renderArm(EnumHandSide.RIGHT);
+            renderArm(EnumHandSide.LEFT);
+            GlStateManager.popMatrix();
+            GlStateManager.enableCull();
+        }
+    }
+    private void renderArm(EnumHandSide p_187455_1_)
+    {
+        Minecraft.getMinecraft().getTextureManager().bindTexture(Minecraft.getMinecraft().thePlayer.getLocationSkin());
+        Render<AbstractClientPlayer> render = Minecraft.getMinecraft().getRenderManager().<AbstractClientPlayer>getEntityRenderObject(Minecraft.getMinecraft().thePlayer);
+        RenderPlayer renderplayer = (RenderPlayer)render;
+        GlStateManager.pushMatrix();
+        float f = p_187455_1_ == EnumHandSide.RIGHT ? 1.0F : -1.0F;
+        GlStateManager.rotate(92.0F, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate(45.0F, 1.0F, 0.0F, 0.0F);
+        GlStateManager.rotate(f * -41.0F, 0.0F, 0.0F, 1.0F);
+        GlStateManager.translate(f * 0.3F, -1.1F, 0.45F);
+
+        if (p_187455_1_ == EnumHandSide.RIGHT)
+        {
+            renderplayer.renderRightArm(Minecraft.getMinecraft().thePlayer);
+        }
+        else
+        {
+            renderplayer.renderLeftArm(Minecraft.getMinecraft().thePlayer);
+        }
+
+        GlStateManager.popMatrix();
+    }
+
+    private void transformEatFirstPerson(float partialTicks, EnumHandSide p_187454_2_)
+    {
+        float f = (float)ticksUsing - partialTicks + 1.0F;
+        float f1 = f / (float)33;//max use duration = 33
+
+        if (f1 < 0.8F)
+        {
+            float f2 = MathHelper.abs(MathHelper.cos(f / 4.0F * (float)Math.PI) * 0.1F);
+            GlStateManager.translate(0.0F, f2, 0.0F);
+        }
+
+        float f3 = 1.0F - (float)Math.pow((double)f1, 27.0D);
+        int i = p_187454_2_ == EnumHandSide.RIGHT ? 1 : -1;
+        GlStateManager.translate(f3 * 0.6F * (float)i, f3 * -0.5F, f3 * 0.0F);
+        GlStateManager.rotate((float)i * f3 * 90.0F, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate(f3 * 10.0F, 1.0F, 0.0F, 0.0F);
+        GlStateManager.rotate((float)i * f3 * 30.0F, 0.0F, 0.0F, 1.0F);
     }
 }
